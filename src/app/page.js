0990b30aa749a61e3c53f401e4711226220dcfbd
Celso <juniorcelso791@gmail.com>
2024-08-29 21:48:10 -0400

    Dark-mode

---
+++ src/app/page.js
@@ -0,0 +1,37 @@
+'use client';
+import { useState } from "react";
+import styles from "./page.module.css";
+
+export default function Home() {
+
+  const [alterarTema, setAlterarTema] = useState(false)
+
+  function trocarTemaPagina() {
+    setAlterarTema(!alterarTema)
+  }
+
+  return (
+    <div className={alterarTema ? styles.dark_mode : styles.light_mode}>
+      <header className={styles.header_container}>
+        <h1>Coffe Shop</h1>
+        <button onClick={trocarTemaPagina}>
+          {alterarTema ? 'Sol' : 'Lua'}
+        </button>
+      </header>
+      <main className={styles.main_container}>
+        <div>
+          <h2>A melhor cafeteria do Brasil</h2>
+          <p>
+            Venha conhecer a melhor cafeteria do Brasil! 
+            Localizada no coração de São Paulo, oferecemos cafés artesanais de alta qualidade e um ambiente acolhedor. 
+            Desfrute de nossas bebidas exclusivas e deliciosos acompanhamentos enquanto relaxa em um espaço único. 
+            Esperamos por você para uma experiência inesquecível!
+          </p>
+          <button>Nossas Lojas</button>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+
